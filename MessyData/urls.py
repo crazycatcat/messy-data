dcac2b64b278109f424c7d66f1dce3b706b4033f
crazycatcat <shrandwky@foxmail.com>
2017-10-09 19:57:38 -0700

    fixed

--- MessyData/urls.py
+++ MessyData/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls',namespace='users')),
     url(r'',include('mainsite.urls',namespace='mainsite')),
-]
+]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
