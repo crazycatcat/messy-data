37cd6e08da1fad9c60939233dab83d74b3b0625e
Xiaozhouluo <34893986@163.com>
2017-10-13 20:13:36 -0700

    add require email

--- users/views.py
+++ users/views.py
@@ -6,22 +6,53 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth import logout,login,authenticate
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 # Create your views here.
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('mainsite:index'))
 
-def register(request):
+'''def register(request):
     if request.method!='POST':
 	form=UserCreationForm()
     else:
 	form=UserCreationForm(data=request.POST)
 	if form.is_valid():
             new_user=form.save()
-	    authenticated_user=authenticate(username=new_user.username,password=request.POST['password1'])
+	    authenticated_user=authenticate(username=new_user.username,email=new_user.email,password=request.POST['password1'])
 	    login(request,authenticated_user)
 	    return HttpResponseRedirect(reverse('mainsite:index'))
 	
     context={'form':form}
-    return render(request,'users/register.html',context)
+    return render(request,'users/register.html',context)'''
+	
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            new_user=form.save()
+            authenticated_user=authenticate(username=new_user.username,email=new_user.email,password=request.POST['password1'])
+            login(request,authenticated_user)
+            return HttpResponseRedirect(reverse('mainsite:index'))
+            
+
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+	
+'''user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('pre_user/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            # user.email_user(subject, message) # 给该用户发送邮件
+            user.email_user(subject=subject, message='message',html_message=message) # 给该用户发送邮件
+            return redirect('account_activation_sent')
+            # return HttpResponse("邮箱已经发送，请前往验证")'''
